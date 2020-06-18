8cd063237b8fd46f0ea7737f4e4426ac641f1800
testowyK <wojcikpawel25@gmail.com>
2020-06-18 13:50:11 +0200

    change project name

--- IntelligenceTest/Properties/AssemblyInfo.cs
+++ IntelligenceTest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntelligenceTest")]
+[assembly: AssemblyTitle("EducationalGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IntelligenceTest")]
+[assembly: AssemblyProduct("EducationalGame")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
