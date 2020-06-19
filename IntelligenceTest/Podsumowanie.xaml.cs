67a4aac6400a78bef71a9861d686eb08f949e663
testowyK <wojcikpawel25@gmail.com>
2020-06-19 17:52:43 +0200

    results

---
+++ IntelligenceTest/Podsumowanie.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace IntelligenceTest
+{
+    public partial class Podsumowanie : PhoneApplicationPage
+    {
+        private int suma = 0;
+        public Podsumowanie()
+        {
+            InitializeComponent();
+
+            suma = Dane.zwrocScoreDzialania() + Dane.zwrocScoreIQ() + Dane.zwrocScoreObrazki();
+            
+            textBlockScoreDzialania.Text = "Score: " + Dane.zwrocScoreDzialania().ToString();            
+            textBlockScoreIQ.Text = "Score: " + Dane.zwrocScoreIQ();
+            textBlockScoreObrazki.Text = "Score: " + Dane.zwrocScoreObrazki();
+            textBlockSumaryczny.Text = "Overall: " + suma;
+
+            if(Dane.zwrocNick() != null && Dane.zwrocNick() != " ")
+            {
+                textBlockNick.Text = "Your nickname: " + Dane.zwrocNick();
+            }
+            else
+            {
+                textBlockNick.Text = "Score default";
+            }
+        }
+
+
+        private void bPowrotClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/WyborGry.xaml", UriKind.Relative));
+        }
+    }
+}
